b2f333f9dc5d4f8bd70ba575eec815af8fd3b475
Keld Marple <kemm@kea.dk>
2025-03-31 12:26:47 +0200

    Adding animation on info-boxes

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ function headUnHighlight() {
 
 function headInfo() {
   console.log("headInfo");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Hovedet!";
   document.querySelector(".info-text > article > p").textContent = "Lorem ipsum dolor sit amet, velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   document.querySelector("#efficiency").innerHTML = "<h3>Effektivitet</h3><p>Afhukning er supereffektivt og et af de bedste passificeringsaktiviteter man kan bruge. Også for zombier!</p>";
@@ -26,6 +29,9 @@ function headInfo() {
 }
 function shoulderInfo() {
   console.log("shoulderInfo");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Skulderen!";
   document.querySelector(".info-text > article > p").textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
   document.querySelector("#efficiency").innerHTML = "<h3>Effektivitet</h3><p>Det er rimelig nyttigt at hugge en arm af en zombie, og skal man gøre det, er skulderen et godt sted at hugge på.";
@@ -33,8 +39,26 @@ function shoulderInfo() {
 }
 function kneeInfo() {
   console.log("kneeInfo");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Knæet!";
   document.querySelector(".info-text > article > p").textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.";
   document.querySelector("#efficiency").innerHTML = "<h3>Effektivitet</h3><p>Ikke super effektivt, da en zombie allerede er langsom, så man får ikke så meget ud af at forstyrre dens balance.";
   document.querySelector("#requirement").innerHTML = "<h3>Styrke</h3><p>Det kræver ikke så meget. Det kan gøres med en træpind.</p>";
+}
+
+function animateBoxes() {
+  document.querySelector("#efficiency").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("fadeIn");
+  document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#requirement").classList.add("fadeIn");
+  document.querySelector("#requirement").addEventListener("animationend", cleanup);
+}
+
+function cleanup() {
+  console.log("cleanup");
+  document.querySelector("#requirement").removeEventListener("animationend", cleanup);
+  document.querySelector("#efficiency").classList.remove("fadeIn");
+  document.querySelector("#requirement").classList.remove("fadeIn");
 }
